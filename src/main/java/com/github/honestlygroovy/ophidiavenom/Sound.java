cd17b53959f66c1fd287718c8942d6611526364d
Juki852 <markgaffin@gmail.com>
2026-06-12 01:31:38 +1200

    Updated sound enums.

--- src/main/java/com/github/honestlygroovy/ophidiavenom/Sound.java
+++ src/main/java/com/github/honestlygroovy/ophidiavenom/Sound.java
@@ -2,40 +2,36 @@ package com.github.honestlygroovy.ophidiavenom;
 
 public enum Sound
 {
-
-	DEATH("death", "DyingHCIMCompleted_r1.wav"),//#2
-	COLLECTION_LOG_SLOT("collectionlog", "ColLogSlotCompleted_r1.wav"),//#3
-	NO_PRAYER("noprayer", "NoPrayer.wav"),//#4
-	NEW_PET("newpet", "NewPet.wav"),//#5
-	PETTING_DOG("pettingdog", "PettingDog.wav"),//#6
-	DECLINE_TRADE("declinetrade", "DeclineTrade.wav"),//#7
-	CRAB_CHECK("crabcheck", "CrabCheck.wav"),//#8
-	HUNTER_RUMOUR("hunterrumour", "HunterRumourCompleted.wav"),//#8
-	HUNTER_RUMOUR_NOT_COMPLETED("hunterrumour", "HunterRumourNotCompleted.wav"),//#8
-	RANGE("range", "Range.wav"),
-	MAGE("mage", "Mage.wav"),
-
-	KILLING_SOMEONE_1("playerkilling", "KillingSomeone_r1.wav"),//#9
-
-	COMBAT_TASK("combattask", "CombatTaskCompleted_r1.wav"),//#15
-
-	ACCEPTED_TRADE("accepttrade", "AcceptTrade.wav"),//#17
-
-	YOUR_PURPLE("gettingpurple", "GettingPurple_r1.wav"),//#18
-	NOT_YOUR_PURPLE("notgettingpurple", "NotGettingPurple.wav"),//#18
-
-	DISMISSING_RANDOM_EVENT("dismissrandomevent", "DismissingRandomEvent.wav"),//#20
-	TYPING_IN_BANKPIN("typingbankpin", "TypingInBankpin.wav"),//#21
-	TOA_CHEST_OPENS("toachestopens", "ToaChestOpens.wav"),//#23
-	WHITE_LIGHT_AFTER_RAID("whitelight", "WhiteLightAfterRaid.wav"),//#24
-	CLICKING_PK_LOOT_CHEST("pkchest", "ClickingPkLootChest.wav"),//#26
-
-	LEVEL_UP("levelup", "LevelUpCompleted_r1.wav"),
-	LEVEL_99("level99", "Level99.wav"),
-	QUEST("quest", "QuestCompleted_r1.wav"),
-	ACHIEVEMENT_DIARY("achievementdiary", "AchievementDiary_r1.wav"),
-	EASTER_EGG_STRAYDOG_BONE("givebone", "GiveBone.wav"),
-	GO_LIVE("golive", "go_live.wav");
+	ACCEPTED_TRADE("AcceptTrade", "AcceptTrade.wav"),//#17
+	ACHIEVEMENT_DIARY("AchievementDiaryCompleted", "AchievementDiaryCompleted.wav"),
+	COLLECTION_LOG_SLOT("CollectionLogSlotCompleted", "CollectionLogSlotCompleted.wav"),//#3
+	COMBAT_TASK("CombatAchievementCompleted", "CombatAchievementCompleted.wav"),//#15
+	CRAB_CHECK("CrabCheck", "CrabCheck.wav"),//#8
+	DEATH("Death", "Death.wav"),//#2
+	DECLINE_TRADE("DeclineTrade", "DeclineTrade.wav"),//#7
+	DISMISSING_RANDOM_EVENT("Dismiss", "Dismiss.wav"),//#20
+	TOA_CHEST_OPENS("GoodLuck", "GoodLuck.wav"),//#23
+	HUNTER_RUMOUR("HunterRumourCompleted", "HunterRumourCompleted.wav"),//#8
+	//TODO: Record Hunter Rumour Not Completed
+	HUNTER_RUMOUR_NOT_COMPLETED("HunterRumourNotCompleted", "HunterRumourNotCompleted.wav"),//#8
+	GO_LIVE("ImLive", "ImLive.wav"),
+	LEVEL_99("Level99", "Level99.wav"),
+	LEVEL_UP("levelX", "LevelX.wav"),
+	MAGE("Mage", "Mage.wav"),
+	NEW_PET("NewPet", "NewPet.wav"),//#5
+	NO_PRAYER("NoPrayer", "NoPrayer.wav"),//#4
+	PETTING_DOG("PettingDog", "PettingDog.wav"),//#6
+	CLICKING_PK_LOOT_CHEST("PKChest", "PKChest.wav"),//#26
+	KILLING_SOMEONE_1("PKKill", "PKKill.wav"),//#9
+	YOUR_PURPLE("PurpleChestYours", "PurpleChestYours.wav"),//#18
+	QUEST("QuestCompleted", "QuestCompleted.wav"),
+	RANGE("Range", "Range.wav"),
+	//TODO: Record Slayer Task Completed
+	SLAYER_TASK("SlayerTaskCompleted", "SlayerTaskCompleted.wav"),
+	TYPING_IN_BANKPIN("Snap", "Snap.wav"),//#21
+	WHITE_LIGHT_AFTER_RAID("WhiteChest", "WhiteChest.wav"),//#24
+	//TODO: Record Not Your Purple
+	NOT_YOUR_PURPLE("PurpleChestNotYours", "PurpleChestNotYours.wav");//#18
 
 	private final String resourceName;
 	private final String directory;
